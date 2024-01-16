d885449a717ab9d4cc7de08ee5b7274dbad49832
Deboh12 <deborah.alexander12@yahoo.com>
2024-01-15 23:47:32 -0600

    added comments

--- server.js
+++ server.js
@@ -3,22 +3,27 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3001; 
+const PORT = 3001;
 
+//  ID generation 
 const generateId = () => `${Date.now()}`;
 
+// Middleware for parsing JSON 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// GET Route for retrieving all the notes
 app.get('/api/notes', (req, res) =>
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) {
@@ -29,6 +34,7 @@ app.get('/api/notes', (req, res) =>
   })
 );
 
+// POST Route for a new note
 app.post('/api/notes', (req, res) => {
   const { title, text } = req.body;
 
@@ -38,6 +44,7 @@ app.post('/api/notes', (req, res) => {
 
   const newNote = { title, text, id: generateId() };
 
+  // Read, update, and write the notes data
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) {
       console.error(err);
@@ -54,6 +61,7 @@ app.post('/api/notes', (req, res) => {
   });
 });
 
+// BONUS: DELETE Route for a specific note
 app.delete('/api/notes/:id', (req, res) => {
   const noteId = req.params.id;
 
@@ -72,6 +80,7 @@ app.delete('/api/notes/:id', (req, res) => {
   });
 });
 
+// Listen on PORT
 app.listen(PORT, () =>
   console.log(`Note Taker server is listening on port ${PORT}`)
 );
